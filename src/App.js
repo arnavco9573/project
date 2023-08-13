7187c39755263a371910a0e4a8b8fcd30fa7412b
arnavco9573 <arnavsharm589@gmail.com>
2023-08-13 23:23:06 +0530

    init first file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Accordian from "./components/Accordian";
+import BoxSec from "./components/BoxSec";
+import CardHero from "./components/CardHero";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Join from "./components/Join";
+import Navbar from "./components/Navbar";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <CardHero />
+      <BoxSec />
+      <Testimonials />
+      <Accordian/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }
